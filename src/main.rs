e6086ee4782acb990e6570de90543634711acc56
PhiZero <kanta03085@outlook.jp>
2020-10-07 00:36:22 +0900

    Split every 20 tweets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate reqwest;
 use reqwest::Client;
 use serde::Deserialize;
+use url::Url;
 
 // user name
 const SCREEN_NAME: &str = "Kanta_phi";
@@ -42,12 +43,46 @@ struct UserMention {
     screen_name: String,
 }
 
+use anyhow::Result;
+
+async fn get_tweets(max_id: Option<u64>) -> Result<Vec<Tweet>> {
+    let mut params = vec![
+        ("screen_name", SCREEN_NAME),
+        ("count", "20"),
+    ];
+    let id;
+    if let Some(max_id) = max_id {
+        id = max_id.to_string();
+        params.push(("max_id", &id));
+    }
+
+    let url = Url::parse_with_params("https://api.twitter.com/1.1/statuses/user_timeline.json", params)?;
+    let token = std::env::var("TOKEN")?;
+    let token = format!("Bearer {}", token);
+    let client = Client::new().get(url).header("authorization", token);
+    let tweets = client.send().await?.json::<Vec<Tweet>>().await?;
+    Ok(tweets)
+}
+
 #[tokio::main]
 async fn main() {
-    let url = format!("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}", SCREEN_NAME);
-    let token = std::env::var("TOKEN").unwrap();
-    let token = format!("Bearer {}", token);
-    let client = Client::new().get(&url).header("authorization", token);
-    let res = client.send().await.unwrap().json::<Vec<Tweet>>().await.unwrap();
-    println!("{:#?}", res);
+    let mut last_id = None;
+    for n in 0..5 {
+        match get_tweets(last_id).await {
+            Ok(tweets) => {
+                if tweets.len() == 0 {
+                    break;
+                }
+                println!("=====================================");
+                println!("page: {}", n+1);
+                println!("=====================================");
+                println!("{:#?}", tweets);
+                last_id = Some(tweets.last().unwrap().id-1);
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                break;
+            }
+        }
+    }
 }
