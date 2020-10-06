ad1ab3e97dab421f41cf1fd43f023d627bca4da2
PhiZero <kanta03085@outlook.jp>
2020-10-06 23:30:53 +0900

    show twitter data from environment variable token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+use reqwest::Client;
+
+// user name
+const SCREEN_NAME: &str = "Kanta_phi";
+
+#[tokio::main]
+async fn main() {
+    let url = format!("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}", SCREEN_NAME);
+    let token = std::env::var("TOKEN").unwrap();
+    let token = format!("Bearer {}", token);
+    let client = Client::new().get(&url).header("authorization", token);
+    let res = client.send().await.unwrap().text().await.unwrap();
+    println!("{}", res);
 }
